d74000871fe8cb2df8944a8985942a37f2fdd0bd
Tando Mahlati <116363298+ButiTando@users.noreply.github.com>
2024-02-13 19:05:26 +0000

    Updates to code

--- to-do-list/app/page.tsx
+++ to-do-list/app/page.tsx
@@ -1,11 +1,38 @@
 import Image from "next/image";
+import ListItem from "@/components/listItem"
 
 export default function Home() {
   return (
-    <>
-      <div>
-        Hello world
+    <div className="flex flex-col
+                    h-screen w-full">
+      <div className="h-[2.25em] w-full
+                      flex flex-row
+                      items-center
+                      bg-green-500 
+                      text-red-500 text-2xl
+                      relative
+                      ">
+        To do list
       </div>
-    </>
+
+      {/* This should be displayed on small screens */}
+      <ul className="w-full 
+                      flex-1
+                     bg-red-300
+                     ">
+        
+        <ListItem/>
+
+      </ul>
+
+      {/* This should be displayed on medium screens */}
+      <ul className="sm:hidden">
+          
+      </ul>
+
+      {/* This should be displayed on large screens  */}
+      <ul className="sm:hidden"></ul>
+
+    </div>
   );
 }
---
+++ to-do-list/components/listItem.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const ListItem = (): JSX.Element => {
+
+    return (
+        <li className="w-full h-[2.5em]
+                       bg-slate-200
+                       flex flex-row
+                       items-center
+                       mt-[4px]
+                        ">
+            Hello world
+        </li>
+    )
+}; 
+
+export default ListItem;
